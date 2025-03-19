c073395ef0fdbd6ca21eca9db23c788f7035ed8d
iw-00 <133152066+iw-00@users.noreply.github.com>
2025-03-19 16:24:22 -0400

    add data

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /*------------------------------------------------------------------------------
-GGR472 Lab 3: Adding styling and interactivity to web maps with JavaScript.
+GGR472 Project: 
 --------------------------------------------------------------------------------*/
 
 // Add default public map token from Mapbox.
 mapboxgl.accessToken = "pk.eyJ1IjoiaXcwMCIsImEiOiJjbTV2aXFlajYwMjZmMmtvbWtrMGRhd3lkIn0.DbEVxhgWv4ANYwpIpCc4iA"; 
 
+// Create map
 const map = new mapboxgl.Map({
     container: 'map',
     projection: 'mercator',
@@ -28,11 +29,34 @@ const geocoder = new MapboxGeocoder({
 // Append geocoder variable to goeocoder HTML div to position on page
 document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
 
+// Add data layers to map.
+// ------------------------------------------------------------
+map.on("load", () => {
+
+  // Add source for show data
+  map.addSource("show-data", {
+      type: "geojson",
+      data: "data/shows.geojson"
+  });
+
+  // Add stadium points to map
+  map.addLayer({
+        id: "show-pts",
+        type: "circle",
+        source: "show-data",
+        paint: {
+            "circle-radius": 4,
+            "circle-color": "#1ff258"
+        }
+    });
+});
+
   // add function to the return button with flyto, bringing us back to our original zoom and center point
 document.getElementById('returnbutton').addEventListener('click', () => {
   map.flyTo({
-      center: [3.703304, 37.708399],
-      zoom: 5,
+      center: [2.340180, 26.389773],
+      zoom: 1.8,
       essential: true
   });
 });
+
